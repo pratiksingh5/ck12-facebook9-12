181653bb00e2055be1de353027450e4227ece65b
Pratik <pratikrajaryan@gmail.com>
2021-09-23 12:38:05 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const userModel = require("./users");
+const postModel = require('./post')
+const { body, validationResult } = require('express-validator');
 
 const data = new userModel()
 
@@ -11,10 +13,18 @@ passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.render("index");
+  res.render("index",{errors:false});
 });
 
-router.post("/reg", function (req, res) {
+router.post("/reg", body('password').isLength({ min: 5 }).withMessage("Password should be of minimum 5 characters"),
+function (req, res) {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // return res.status(400).json({ errors: errors.array() });
+    res.render('index',errors)
+  }
+
   const dets = new userModel({
     name: req.body.name,
     username: req.body.username,
@@ -63,5 +73,32 @@ function isLoggedIn(req, res, next) {
     res.redirect("/login");
   }
 }
+
+router.get("/timeline", function (req, res) {
+  userModel.findOne({username:req.session.passport.user}).populate('posts')
+  .then(function(foundUser){
+    res.render("timeline",{foundUser});
+
+  })
+});
+
+
+
+router.post('/post', function (req, res) {
+  userModel.findOne({ username: req.session.passport.user })
+    .then(function (foundUser) {
+      postModel.create({
+        postText: req.body.post,
+        user: foundUser
+      }).then(function (newlyCreatedPost) {
+        foundUser.posts.push(newlyCreatedPost);
+        foundUser.save().then(function () {
+          res.redirect('/timeline');
+        })
+      })
+    })
+});
+
+
 
 module.exports = router;
